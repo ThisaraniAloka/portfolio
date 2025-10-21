64a1646439292f3570f4eb3cc81029b1ec10ec7b
ThisaraniAloka <164716628+ThisaraniAloka@users.noreply.github.com>
2025-10-21 12:35:35 +0530

    update

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -25,128 +25,151 @@ const mywork_data = [
         w_no:1,
         w_name:"Car Rental Web",
         w_img:project1_img,
-        github: "https://github.com/ThisaraniAloka/carRentWeb.git"
+        github: "https://github.com/ThisaraniAloka/carRentWeb.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_mern-reactjs-nodejs-activity-7364144994175078403-1n6U?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:1,
         w_name:"hand sign recognize mobile app",
         w_img:project21_img,
-        github: "https://github.com/NavinduR2001/hand-sign-recognize-mobile-app.git"
+        github: "https://github.com/NavinduR2001/hand-sign-recognize-mobile-app.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_capstoneproject-wavewords-ai-activity-7373532419251097600-uJ5P?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:1,
         w_name:"Keyboard Website",
         w_img:project20_img,
-        github: "https://github.com/ThisaraniAloka/KeyboardWebsite.git"
+        github: "https://github.com/ThisaraniAloka/KeyboardWebsite.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_nextjs-threejs-gsap-activity-7381656163790020610-awv1?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
+        
     },
     {
         w_no:2,
         w_name:"Veterinary Management System",
         w_img:project2_img,
-        github: "https://github.com/KKShaminda/Veterinary-Reports-Management-System.git"
+        github: "https://github.com/KKShaminda/Veterinary-Reports-Management-System.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_webdevelopment-php-html-activity-7235541672443400194-b463?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:3,
         w_name:"Online Clothing Shop",
         w_img:project3_img,
-        github: "https://github.com/ThisaraniAloka/Online-Clothing-Shop.git"
+        github: "https://github.com/ThisaraniAloka/Online-Clothing-Shop.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_webdevelopment-ecommerce-html-activity-7235539013313380352-hiBM?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:4,
         w_name:"MERN-E-commerce",
         w_img:project19_img,
         github: "https://github.com/ThisaraniAloka/MERN-E-commerce.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_mern-ecommerce-reactjs-activity-7359096591456063489-Ggwx?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:5,
         w_name:"Chess Game",
         w_img:project5_img,
-        github: "https://github.com/ThisaraniAloka/ChessGame.git"
+        github: "https://github.com/ThisaraniAloka/ChessGame.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_webdevelopment-javascript-html-activity-7240640326233677824-R1BA?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
+    },
+    {
+        w_no:17,
+        w_name:"ChatApp",
+        w_img:project18_img,
+        github: "https://github.com/ThisaraniAloka/ChatApp.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_mern-chatapp-socketio-activity-7325549641817587712-lDdq?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:6,
         w_name:"Travel and Tour website",
         w_img:project6_img,
-        github: "https://github.com/ThisaraniAloka/Travel-and-Tour-website.git"
+        github: "https://github.com/ThisaraniAloka/Travel-and-Tour-website.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_webdevelopment-php-javascript-activity-7242203873891811330-siAd?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:7,
         w_name:"chatbot",
         w_img:project4_img,
-        github: "https://github.com/ThisaraniAloka/Chatbot.git"
+        github: "https://github.com/ThisaraniAloka/Chatbot.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_ai-chatbot-javascript-activity-7235544677536423938-GjJ9?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:8,
         w_name:"Snake Game",
         w_img:project8_img,
-        github: "https://github.com/ThisaraniAloka/Snake-Game.git"
+        github: "https://github.com/ThisaraniAloka/Snake-Game.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_my-classic-snake-game-in-java-excited-activity-7271171104767942656-jkRQ?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:9,
         w_name:"Campus Website",
         w_img:project9_img,
-        github: "https://github.com/ThisaraniAloka/CampusWebsite-ReactJS.git"
+        github: "https://github.com/ThisaraniAloka/CampusWebsite-ReactJS.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_introducing-eduverse-a-reactjs-university-activity-7271744226235703296-k8ZT?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:10,
         w_name:"Todo App",
         w_img:project10_img,
-        github: "https://github.com/ThisaraniAloka/Todo_App.git"
+        github: "https://github.com/ThisaraniAloka/Todo_App.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_nextjs-mongodb-fullstack-activity-7273730286369054720-3j1X?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:11,
         w_name:"Quiz App",
         w_img:project11_img,
-        github: "https://github.com/ThisaraniAloka/QuizApp.git"
+        github: "https://github.com/ThisaraniAloka/QuizApp.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_reactnative-mobileappdevelopment-quizapp-activity-7292825381739368448-MSfD?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:12,
         w_name:"E-Commerce App",
         w_img:project12_img,
-        github: "https://github.com/ThisaraniAloka/E-CommerceApp.git"
+        github: "https://github.com/ThisaraniAloka/E-CommerceApp.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_reactnative-mobileapp-ecommerce-activity-7296196473006809088-3XEE?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:13,
         w_name:"Apple Catcher Game",
         w_img:project13_img,
-        github: "https://github.com/ThisaraniAloka/AppleCatcherGame.git" 
+        github: "https://github.com/ThisaraniAloka/AppleCatcherGame.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_python-gamedevelopment-coding-activity-7300158515040374785-n7Ni?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE" 
     },
     {
         w_no:14,
         w_name:"Calculator",
         w_img:project7_img,
-        github: "https://github.com/ThisaraniAloka/Java-Calculator.git"
+        github: "https://github.com/ThisaraniAloka/Java-Calculator.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_javaprogramming-swing-gui-activity-7269613172934746112-LDvU?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:15,
         w_name:"Image Filter",
         w_img:project14_img,
-        github: "https://github.com/ThisaraniAloka/ImageFilter.git"
+        github: "https://github.com/ThisaraniAloka/ImageFilter.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_python-opencv-imageprocessing-activity-7301447084841254913-T0L9?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:16,
         w_name:"Doctor Appointment Web",
         w_img:project15_img,
-        github: "https://github.com/ThisaraniAloka/DoctorAppointmentWeb.git"
+        github: "https://github.com/ThisaraniAloka/DoctorAppointmentWeb.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_reactjs-tailwindcss-webapp-activity-7306571506514374656-_IVQ?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:17,
         w_name:"Chrome Dinosaur Game",
         w_img:project16_img,
-        github: "https://github.com/ThisaraniAloka/ChromeDinosaurGame.git"
+        github: "https://github.com/ThisaraniAloka/ChromeDinosaurGame.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_java-gamedevelopment-swing-activity-7311371827405037568-RrTk?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
     },
     {
         w_no:18,
         w_name:"Product Store Page",
         w_img:project17_img,
-        github: "https://github.com/ThisaraniAloka/ProductStorePage-MERN_Stack.git"
-    },
-    {
-        w_no:17,
-        w_name:"Chatbot",
-        w_img:project18_img,
-        github: "https://github.com/ThisaraniAloka/Chatbot.git"
-    },
+        github: "https://github.com/ThisaraniAloka/ProductStorePage-MERN_Stack.git",
+        demo: "https://www.linkedin.com/posts/thisarani-aloka-aa74a6310_mern-webdev-react-activity-7319001693507969026-caH2?utm_source=share&utm_medium=member_desktop&rcm=ACoAAE8YSJ8B_O9Yhpd1HtwTZqcfVl4YTTjqONE"
+    }
+    
 ]
  
 export default mywork_data;
